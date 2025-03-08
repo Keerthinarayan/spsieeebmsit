4746ef3813bd4dc85d66abac4d3e0210a8ebaf71
Keerthinarayan <149809285+Keerthinarayan@users.noreply.github.com>
2025-03-08 17:32:48 +0000

    github change

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/spsieeebmsit/', // Correct for GitHub Pages deployment
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
